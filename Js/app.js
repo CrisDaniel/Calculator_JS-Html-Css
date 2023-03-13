8542fab12793c2de25f77f805ae7e752c359a519
Cristian <daniel981311@gmail.com>
2023-03-13 18:36:33 -0500

    Calculator

---
+++ Js/app.js
@@ -0,0 +1,8 @@
+function agregar(idNumero) {
+    console.log("Hola");
+    let numero = document.getElementById(idNumero).innerHTML;
+    console.log(numero);
+    console.log("Holaaaaaa");
+    document.getElementById("mostrar").innerHTML = numero.innerHTML;
+    
+}
