8b91954ca3c01810782979ff2c2f6fd538cdb728
Cristian <daniel981311@gmail.com>
2024-02-18 20:38:35 -0500

    Basic calculator completion

--- Js/app.js
+++ Js/app.js
@@ -1,69 +1,80 @@
 let mostrarOperacion = document.getElementById("operacion");
 let mostrarResultado = document.getElementById("resultado");
-let firstValor, secondValor, arrayNum;
+const buttonContainer = document.getElementById("buttonContainer");
+const button = buttonContainer.querySelector("button");
+let firstValor,
+  secondValor,
+  arrayNum,
+  encendido = false;
 
-function agregar(idNumero) {
-  mostrarOperacion.innerHTML == "0"
-    ? (mostrarOperacion.innerHTML = idNumero.innerHTML)
-    : (mostrarOperacion.innerHTML += idNumero.innerHTML);
-}
+function prender() {
+  encendido = !encendido;
+  button.textContent = encendido ? "ON" : "OFF";
+  button.classList.toggle("buttonOn");
+  button.classList.toggle("buttonOff");
 
-function mostrar(resultado) {
-  mostrarResultado.innerHTML = resultado;
-}
+  if (encendido) {
+    window.agregar = function agregar(idNumero) {
+      mostrarOperacion.innerHTML == "0"
+        ? (mostrarOperacion.innerHTML = idNumero.innerHTML)
+        : (mostrarOperacion.innerHTML += idNumero.innerHTML);
+    }
 
-function limpiar() {
-  mostrarOperacion.innerHTML = "0";
-  mostrarResultado.innerHTML = "0";
-}
+    window.mostrar = function mostrar(resultado) {
+      mostrarResultado.innerHTML = resultado;
+    }
 
-function prender(id) {
-  console.log(id.innerHTML);
-}
+    window.limpiar = function limpiar() {
+      mostrarOperacion.innerHTML = "0";
+      mostrarResultado.innerHTML = "0";
+    }
 
-function apagar(id) {
-  console.log(id.innerHTML);
-}
+    window.borrar = function borrar(idBorrar) {
+      if (mostrarOperacion.innerHTML.length == 1) {
+        limpiar();
+      } else {
+        mostrarOperacion.innerHTML = mostrarOperacion.innerHTML.slice(0, -1);
+      }
+    }
 
-function borrar(idBorrar) {
-  if (mostrarOperacion.innerHTML == "") {
-    limpiar();
+    window.resolver = function resolver() {
+      arrayNum = mostrarOperacion.innerHTML.split("");
+      let operadores = ["+", "-", "*", "/"];
+
+      let operador = arrayNum
+        .filter((char) => operadores.includes(char))
+        .join("");
+      let indice = arrayNum.indexOf(operador);
+      firstValor = parseFloat(arrayNum.slice(0, indice).join(""), 10);
+      secondValor = parseFloat(arrayNum.slice(indice + 1).join(""), 10);
+
+      switch (operador) {
+        case "+":
+          let suma = firstValor + secondValor;
+          mostrar(suma);
+          break;
+        case "-":
+          let resta = firstValor - secondValor;
+          mostrar(resta);
+          break;
+        case "*":
+          let mul = firstValor * secondValor;
+          mostrar(mul);
+          break;
+        case "/":
+          let div = firstValor / secondValor;
+          mostrar(div);
+          break;
+        default:
+          break;
+      }
+    }
   } else {
-    let newArrayNum = mostrarOperacion.innerHTML.slice(
-      0,
-      mostrarOperacion.innerHTML.length - 1
-    );
-    mostrarOperacion.innerHTML = newArrayNum;
-  }
-}
-
-function resolver(id) {
-  arrayNum = mostrarOperacion.innerHTML.split("");
-  let operadores = ["+", "-", "*", "/"];
-
-  let operador = arrayNum.filter((char) => operadores.includes(char)).join("");
-  let indice = arrayNum.indexOf(operador);
-  firstValor = parseFloat(arrayNum.slice(0, indice).join(""), 10);
-  secondValor = parseFloat(arrayNum.slice(indice + 1).join(""), 10);
-
-  switch (operador) {
-    case "+":
-      let suma = firstValor + secondValor;
-      mostrar(suma);
-      break;
-    case "-":
-      let resta = firstValor - secondValor;
-      mostrar(resta);
-      break;
-    case "*":
-      let mul = firstValor * secondValor;
-      mostrar(mul);
-      break;
-    case "/":
-      let div = firstValor / secondValor;
-      mostrar(div);
-      break;
-    default:
-      break;
+    window.agregar = null;
+    window.mostrar = null;
+    limpiar();
+    window.limpiar = null;
+    window.borrar = null;
+    window.resolver = null;
   }
 }
